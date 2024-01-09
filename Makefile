ed97d3d6a8ed7624ba85f4df55a132f254b0c509
Martinus Werts <martinus.werts@univ-angers.fr>
2024-01-09 11:53:49 +0100

    genzignor generates a file with normal random nums

--- Makefile
+++ Makefile
@@ -4,25 +4,30 @@ CC       = gcc
 CFLAGS   = -m64 -O2 -Wall -I.
 CPATH    = .
 
+
 %.o : $(CPATH)/%.c  
 	$(CC) -c $(CFLAGS) $<
 
+
 OBJECTS0 = zigrandom.o zignor.o
-OBJECTS1 = zigtimer.o test_timings.o
+OBJECTS1 = zigtimer.o
 OBJECTS2 = test_moments.o
 OBJECTS3 = test_seed.o
 OBJECTS4 = test_minimal.o
 OBJECTS5 = test_histogram.o
+OBJECTS6 = test_timings.o
+OBJECTS7 = genzignor.o
 
-compile : $(OBJECTS0) $(OBJECTS1) $(OBJECTS2) $(OBJECTS3) $(OBJECTS4) $(OBJECTS5)
+compile : $(OBJECTS0) $(OBJECTS1) $(OBJECTS2) $(OBJECTS3) $(OBJECTS4) $(OBJECTS5) $(OBJECTS6) $(OBJECTS7)
 
 link :
-	$(CC) -o test_timings.exe $(OBJECTS0) $(OBJECTS1) -lm
+	$(CC) -o test_timings.exe $(OBJECTS0) $(OBJECTS1) $(OBJECTS6) -lm
 	$(CC) -o test_moments.exe $(OBJECTS0) $(OBJECTS2) -lm
 	$(CC) -o test_seed.exe $(OBJECTS0) $(OBJECTS3) -lm
 	$(CC) -o test_minimal.exe $(OBJECTS0) $(OBJECTS4) -lm
 	$(CC) -o test_histogram.exe $(OBJECTS0) $(OBJECTS5) -lm
+	$(CC) -o genzignor.exe $(OBJECTS0) $(OBJECTS1) $(OBJECTS7) -lm
 
 clean :
 	rm *.o
-	rm test_timings.exe test_moments.exe test_seed.exe test_minimal.exe test_histogram.exe
+	rm test_timings.exe test_moments.exe test_seed.exe test_minimal.exe test_histogram.exe genzignor.exe
---
+++ genzignor.c
@@ -0,0 +1,71 @@
+/*
+
+genzignor.c
+
+Generate a binary file containing normally N(0,1) distributed random numbers
+
+*/
+
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "zigtimer.h"
+#include "zignor.h"
+
+#define FNAMEMAX 63
+
+int main(int argc, char **argv)
+{
+	char fname[FNAMEMAX+1];
+	int zigseed;
+	long long int i, Nsamples;
+	double *dbuf;
+	FILE *fp;
+	
+	switch(argc)
+	{
+		case 4:
+			zigseed = atoi(argv[1]);
+			Nsamples = atoll(argv[2]);
+			strncpy(fname, argv[3], FNAMEMAX);
+			break;
+		default:
+			printf("ERROR. Unexpected number of arguments.\n");
+			printf("usage: %s <seed> <Nsamples> <filename>\n", argv[0]);
+			return(1);
+	}
+
+	printf("\nGENZIGNOR v1.0\n");
+	printf("-------------------------------------\n");	
+	printf("seed (int32)      : %d\n", zigseed);
+	printf("Nsamples (int64)  : %lld\n", Nsamples);
+	printf("output file       : %s\n", fname);
+	printf("-------------------------------------\n");
+	
+	RanNormalSetSeedZig(&zigseed, 1);
+	
+	dbuf = malloc(sizeof(*dbuf) * Nsamples);
+	
+	StartTimer();
+	for (i = 0; i < Nsamples; i++)
+		dbuf[i] = DRanNormalZig();
+	StopTimer();
+	
+	printf("Random generation : %s seconds\n", GetLapsedTime());
+	printf("-------------------------------------\n");	
+	
+	StartTimer();
+	fp = fopen(fname, "wb");
+	fwrite(dbuf, sizeof(*dbuf), Nsamples, fp);	
+	fclose(fp);
+	StopTimer();
+	
+	printf("File output       : %s seconds\n", GetLapsedTime());
+	printf("-------------------------------------\n\n");	
+	
+	free(dbuf);
+	
+	return 0;
+}
